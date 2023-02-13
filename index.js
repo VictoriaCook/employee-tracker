413c595b0f779479589bb33ca5f9cc381b66fc99
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-02-13 17:36:45 +1100

    added notes

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const inquirer = require('inquirer');
+
+// set up console.table
+
+// set up and connect MySQL database (change root password first)
+
+// store SQL functions in a separate file
+
+// call them in a switch statement based on user selection
